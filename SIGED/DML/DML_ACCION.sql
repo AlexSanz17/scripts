8bd253ba1f2dae95cfc718104943ad6902ad51d4
vcupe <vidalcq2607@gmail.com>
2021-10-21 16:40:32 -0500

    Actualizacion de scripts DDL y DML

---
+++ SIGED/DML/DML_ACCION.sql
@@ -0,0 +1,27 @@
+
+--select * from accion;
+
+insert into accion(nombre,descripcion,estado) values('"archivar"','Archivar','A');
+insert into accion(nombre,descripcion,estado) values('anular','Anular','A');
+insert into accion(nombre,descripcion,estado) values('atender','Atender','A');
+insert into accion(nombre,descripcion,estado) values('responder','Responder','A');
+insert into accion(nombre,descripcion,estado) values('consultar','Consultar','A');
+insert into accion(nombre,descripcion,estado) values('reenviar','Consultar','A');
+insert into accion(nombre,descripcion,estado) values('derivar','Consultar','A');
+insert into accion(nombre,descripcion,estado) values('aprobarqas','Aprobar QAS','A');
+insert into accion(nombre,descripcion,estado) values('rechazar','Rechazar','A');
+insert into accion(nombre,descripcion,estado) values('rechazaruser','Rechazar user','A');
+insert into accion(nombre,descripcion,estado) values('rechazarqas', 'Rechazo QAS', 'A');
+insert into accion(nombre,descripcion,estado) values('registrar','Registrar','A');
+insert into accion(nombre,descripcion,estado) values('aprobar','Aprobar','A');
+insert into accion(nombre,descripcion,estado) values('paraaprobar','Para aprobar','A');
+insert into accion(nombre,descripcion,estado) values('digitalizar','Digitalizar','A');
+insert into accion(nombre,descripcion,estado) values('reabrir','Reabrir','A');
+insert into accion(nombre,descripcion,estado) values('oefa','Enviar a OEFA','A');
+insert into accion(nombre,descripcion,estado) values('copia', 'Copia', 'A');
+insert into accion(nombre,descripcion,estado) values('apoyo','Apoyo','A');
+insert into accion(nombre,descripcion,estado) values('delegar','Delegar','A');
+insert into accion(nombre,descripcion,estado) values('envio_mensajeria','Envio mensajeria','A');
+insert into accion(nombre,descripcion,estado) values('recepcion_cargo','Recepcion cargo','A');
+insert into accion(nombre,descripcion,estado) values('despachar','Despachar','A');
+insert into accion(nombre,descripcion,estado) values('notificar','Notificar','A');
